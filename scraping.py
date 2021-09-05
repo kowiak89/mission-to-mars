89d4ac195d3f0a2bc4b08e45f194b54c4acc640d
David Szymkowiak <davidszymkowiak@Davids-MA.hsd1.or.comcast.net>
2021-09-05 16:27:18 -0700

    d

--- scraping.py
+++ scraping.py
@@ -11,6 +11,7 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
+    hemispheres = mars_hemispheres()
 
     # Run all scraping functions and store results in dictionary
     data = {
@@ -19,6 +20,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now()
+        [hemispheres]
     }
 
     # Stop webdriver and return data
@@ -103,6 +105,28 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def mars_hemispheres():
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+
+    # Grab the entire class that holds all of the hemisphere images
+    div_item = hemi_soup.find('div', class_='collapsible results') 
+
+    hemispheres = []
+
+    for x in div_item.find_all('div', class_='item'):
+        image = x.find('img', class_='thumb').get('src')
+        title = x.find('h3').get_text()
+        hemispheres.append({'img_url':image, 'title':title})
+
+    return hemispheres
 
 if __name__ == "__main__":
     # If running as script, print scraped data
